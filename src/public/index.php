9a1e78bf6085bc266a31309455e985593b461699
AlenaHariel <carneiro.y@escolar.ifrn.edu.br>
2023-04-26 11:15:12 -0300

    kw

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,38 @@
 <?php
+$saldo = 10;
+if ($saldo<100)
+{
+    $deposito = 1000;
+    $saldo += $deposito;
+}
 
-phpinfo();
+echo $saldo;
+
+/*echo 'a: ['.TRUE.']<br/>';
+echo 'a: ['.FALSE.']<br/>';
+echo 'a:['.(20>9).']<br/>';
+echo 'b:['.(5 == 6).']<br/>';
+echo 'c:['.(1 == 0).']<br/>';
+echo 'd:['.(1 === '1').']<br/>';
+$idade = 30;
+echo 'e:['."$idade".']<br/>';
+
+$time = array('Maria','jose','ana');
+echo $time[2];
+echo '<br/>';
+print_r($time);
+//declare(strict_types=1);
+$contador =1;
+$mensagem = "ola";
+$versao = array('1.1','2','3.4');
+print_r($versao);
+$primeiro_nome = "Mariel";
+echo $primeiro_nome;
+echo '<br/>';
+$ultimo_nome = "Alack";
+echo $ultimo_nome;
+
+//phpinfo():*/
+
+
+//echo 'oi';
